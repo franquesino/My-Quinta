ca95797888bcf2f372f110228c4f6d31fd47f26b
franquesino <you@example.com>
2025-02-21 18:26:27 -0300

    Primer commit agrega animacion y hello world

---
+++ app/src/main/java/com/example/myquintaapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.myquintaapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.airbnb.lottie.LottieAnimationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val animationView = findViewById<LottieAnimationView>(R.id.lottieAnimationView)
+        animationView.setAnimation(R.raw.retail_animation)
+        animationView.playAnimation()
+    }
+}
